9af47c92ec2320bf746da6fddf63ed3f3e0e66a0
chaimalk <108950943+chaimalk@users.noreply.github.com>
2023-02-20 04:40:30 +0100

    Update pixel.ads

--- pixel.ads
+++ pixel.ads
@@ -4,7 +4,7 @@
     "admobBanner": "ca-app-pub-1010770342437944/1711000865",
     "admobInterstitial": "ca-app-pub-1010770342437944/2002584845",
     "applovinBanner": "68f4d4be6a6a26f9",
-    "applovinInterstitial": "4d8fe9a0e3c09fd3",
+    "applovinInterstitial": "5f1493409a821dc3",
     "unityGameId": "5172416",
     "testMode": true,
     "unityBanner": "Banner_Android",
