779cc1100d1a02a0bb8b0eb555b6aaea049875b1
chaimalk <108950943+chaimalk@users.noreply.github.com>
2023-02-20 04:37:06 +0100

    Create pixel.ads

---
+++ pixel.ads
@@ -0,0 +1,14 @@
+{
+  "AdsController": {
+    "adNetwork": "unity",
+    "admobBanner": "ca-app-pub-1010770342437944/1711000865",
+    "admobInterstitial": "ca-app-pub-1010770342437944/2002584845",
+    "applovinBanner": "68f4d4be6a6a26f9",
+    "applovinInterstitial": "4d8fe9a0e3c09fd3",
+    "unityGameId": "5172416",
+    "testMode": true,
+    "unityBanner": "Banner_Android",
+    "unityInterstitial": "Interstitial_Android",
+    "reviewActive": true
+  }
+}
